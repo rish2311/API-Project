b62fdee638ee352feabfca08c731d76c142447d3
Rishabh Mehrotra <mehrotrarishabh100@gmail.com>
2025-01-23 21:53:19 +0530

    added images

--- weather_app/src/App.jsx
+++ weather_app/src/App.jsx
@@ -1,5 +1,6 @@
 import Search from './components/search/search.js'
 import './App.css'
+import CurrentWeather from './components/current-weather/current-weather.js';
 
 function App() {
   const handleOnSearchChange = (searchData) => {
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className='container'>
       <Search onSearchChange={handleOnSearchChange}/>
+      <CurrentWeather/>
     </div>
   );
 }
